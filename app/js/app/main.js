62d65f4190249ef5889060ebebc94adb443a68bc
Klaster_1 <Klaster1@gmail.com>
2013-10-13 21:14:17 +0800

    Initial hotkeys implementation

--- app/js/app/main.js
+++ app/js/app/main.js
@@ -6,12 +6,15 @@ require.config({
 		angularui: "lib/angular-ui/angular-ui",
 		angularuirouter: "lib/angular-ui-router/angular-ui-router",
 		angularbs: "lib/angular-bootstrap/ui-bootstrap-0.6.0-SNAPSHOT",
+		mousetrap: "lib/mousetrap/mousetrap.min",
+		mousetrapdict: "lib/mousetrap/mousetrap-bind-dictionary.min",
 		timer: "app/app"
 	},
 	shim: {
 		angularui: ["angular", "jquery"],
 		angularuirouter: ["angular"],
-		angularbs: ["angular"]
+		angularbs: ["angular"],
+		mousetrapdict: ["mousetrap"]
 	}
 });
 
